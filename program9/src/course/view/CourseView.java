c8caa856ed19dc95ab111813d845e0d0b921eff9
Esteban Retana <netherblade619@gmail.com>
2019-12-05 12:33:10 -0700

    added serialization

--- program9/src/course/view/CourseView.java
+++ program9/src/course/view/CourseView.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 
 public class CourseView extends JComponent implements Runnable{
     private String pathName;
--- program9/src/course/view/PushClient.java
+++ program9/src/course/view/PushClient.java
@@ -1,11 +1,57 @@
 package course.view;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
-public class PushClient {
-    public static void main(String[] args) throws IOException {
-        Socket s = new Socket("localhost", 8888);
+public class PushClient implements Runnable{
+    private InputStreamReader streamReader;
+    private BufferedReader clientReader;
+    private PrintWriter clientWriter;
+    private Socket socket;
+    private boolean initilized = false;
+
+    private void initSocket(){
+        try{
+            socket = new Socket("127.0.0.1", 8888);
+            streamReader = new InputStreamReader(socket.getInputStream());
+            clientReader = new BufferedReader(streamReader);
+            clientWriter = new PrintWriter(socket.getOutputStream());
+            initilized = true;
+        }
+        catch(UnknownHostException e){
+            System.err.println("UnknownHostException");
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            System.err.println("IOException");
+            e.printStackTrace();
+        }
+
+    }
+
+    private boolean isInitilized(){
+        return initilized;
+    }
+
+    private String getXMLPackage(){
+        return "";
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable client = new PushClient();
+        Thread clientThread = new Thread(client);
+        clientThread.start();
+
         Runnable view = new CourseView();
 
         Thread viewThread = new Thread(view);
